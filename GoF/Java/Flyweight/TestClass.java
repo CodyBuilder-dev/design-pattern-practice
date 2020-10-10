9969b839b4c52b2aec5fde40ce75509d862181bc
Codybuilder-dev <imspecial1@u.sogang.ac.kr>
2020-10-10 22:21:05 +0900

    [Study] Java Flyweight 패턴

---
+++ GoF/Java/Flyweight/TestClass.java
@@ -0,0 +1,33 @@
+package GoF.Java.Flyweight;
+
+public class TestClass {
+    public static void main(String[] args) {
+        MyData md1 = new MyData();
+        md1.xPos = 1;
+        md1.yPos = 2;
+        md1.name = "홍길동";
+
+        MyData md2 = new MyData();
+        md2 = md1;
+
+        MyData md3 = new MyData();
+        md3.xPos = 12;
+        md3.yPos = 23;
+        md3.name = "홍길동";
+
+        System.out.println(System.identityHashCode(md1.name));
+        System.out.println(System.identityHashCode(md3.name));
+
+        md2.name = "손오공";
+        md2.xPos = 5;
+
+        
+        System.out.println("클래스 테스트!");
+    }
+}
+
+class MyData {
+    int xPos;
+    int yPos;
+    String name;
+}
---
+++ GoF/Java/Flyweight/TestString.java
@@ -0,0 +1,30 @@
+package GoF.Java.Flyweight;
+
+public class TestString {
+    public static void main(String[] args) {
+        // String 생성자를 이용해 같은 내용, 다른 객체 선언
+        // 결과 : 다른 인스턴스
+        String str1 = new String("홍길동");
+        String str2 = new String("홍길동");
+        // 문자열 리터럴을 이용해 같은 애용, 다른 객체 선언
+        // 결과 : 같은 인스턴스(Flyweight 패턴)
+        String str3 = "홍길동";
+        String str4 = "홍길동";
+
+        // 각 개체의 주소를 찾아보자
+        System.out.println(System.identityHashCode(str1));
+        System.out.println(System.identityHashCode(str2));
+        System.out.println(System.identityHashCode(str3));
+        System.out.println(System.identityHashCode(str4));
+
+        // 동일성 검사를 해보자
+        if (str1 == str2) System.out.println("1=2");
+        if (str1 == str3) System.out.println("1=3");
+        if (str1 == str4) System.out.println("1=4");
+        if (str2 == str3) System.out.println("3=4");
+        if (str3 == str4) System.out.println("3=4");
+        
+        
+        System.out.println("Flyweight Pattern");
+    }
+}
